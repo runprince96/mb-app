f0b774ce9770e190a930c22591739c071e4604b4
pio <123.com>
2021-07-20 14:46:06 +0900

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+# posts/views.py
+
+from django.shortcuts import render
+
+from django.views.generic import ListView
+
+from .models import Post
+
+
+# Create your views here.
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
